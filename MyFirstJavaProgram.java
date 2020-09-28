2f766922658b4646971275027ffec56d0786fbb3
Java Software <pj.midgard@yahoo.com>
2020-09-28 15:24:09 +0100

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,9 @@
+public class MyFirstJavaProgram {
+ /* This is my first java program.
+ * This will print ‘Hello World' as the output
+ * @author John Snel
+ */
+ public static void main(String []args) {
+ System.out.println("Hello World"); // prints Hello World 
+ } // end of main
+} // end of MyFirstJavaProgram Class
---
+++ MyFirstVariables.java
@@ -0,0 +1,15 @@
+public class MyFirstVariables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 10;
+        int sum = first + second;
+        
+     System.out.println("first="+first+" second="+second);
+        System.out.println("The sum of the first and second number is "+ sum);
+    }
+    
+}
